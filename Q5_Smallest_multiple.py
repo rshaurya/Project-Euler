0b34ff50ee9be3163abc719381255fe82ac22745
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2021-06-01 19:57:20 +0530

    final program of smallest multiple.

--- Q5_Smallest_multiple.py
+++ Q5_Smallest_multiple.py
@@ -1,17 +1,33 @@
 from math import sqrt
 def isPrime(num):
+    if num < 2:
+        return False
+    if num == 2:
+        return True
     x = int(sqrt(num))
     for j in range(2, x + 1):
         if num % j == 0:
             return False
     return True
 
-#assert(isPrime(17))
-
-def lcm_of_range(n):
+def primeFactors(n):
+    factors = []
     for i in range(2, n+1):
         if (isPrime(i)):
-            print(i)
+            factors.append(i)
+    return factors
 
-lcm_of_range(20)
 
+l = []
+factors = primeFactors(20)
+for i in factors:
+    res = 1
+    while res < 20:
+        res = res * i
+    res = res // i
+    l.append(res)
+
+num = 1
+for x in l:
+    num = num * x
+print(num)
